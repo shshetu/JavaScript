0a9ffae7d0aee7c8b1a0f0e6451f4f51254f5b7c
shshetu <shshetu2017@gmail.com>
2018-10-29 18:34:07 +0600

    JS

---
+++ Lectures/L-36(13-10-18)/test.js
@@ -0,0 +1,28 @@
+//<script>	
+<!--Helloworld.js-->
+document.write("Hello World"+"<br/>"); 
+document.write("<br/>"); 
+<!--print way-->
+var x=10;
+var y=15;
+var result= x+y;
+var str = "Hello world";
+document.write("Result=" + result + "<br/>");
+<!--print way-->
+document.write(str);
+<!--print way-->
+var multi= x*y;
+console.log(multi);
+<!--print way-->
+if(x>y){
+	alert("Belivable Event!");
+} else{
+	alert("Unbelivable Event!");
+}
+var x=500;
+//alert(Take this Money and Give me some food)
+var msg ="Take this money and give me some food";
+alert(x+msg);
+//single line comment
+/*Multiple line comments*/
+//</script>
